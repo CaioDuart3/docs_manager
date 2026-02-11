ee69349f7a06eecc4dfe04010eff8344eba19fd7
caioduart3 <caioduarte330@gmail.com>
2026-02-11 18:32:16 -0300

    feat: adiciona lógica de detalhar documento e comentar

--- docs_manager/documents/views.py
+++ docs_manager/documents/views.py
@@ -7,22 +7,21 @@ def documents_list(request):
     return render(request, 'documents/documents_list.html', {'documents': documents})
 
 def documents_details(request, pk):
-    # document = get_object_or_404(Document, pk=pk)
-    # comments = Comment.objects.filter(document=document)
-    # comment_form = CommentForm()
-    # if request.method == 'POST':
-    #     comment_form = CommentForm(request.POST)
-    #     if comment_form.is_valid():
-    #         comment = comment_form.save(commit=False)
-    #         comment.document = document
-    #         comment.save()
-    #         return redirect('documents_details', pk=document.pk)
-    # return render(request, 'documents/documents_details.html', {
-    #     'documents': document,
-    #     'comments': comments,
-    #     'comment_form': comment_form
-    # })
-    return render(request, 'documents/documents_details.html')
+    document = get_object_or_404(Document, pk=pk)
+    comments = Comment.objects.filter(document=document)
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.document = document
+            comment.save()
+            return redirect('documents_details', pk=document.pk)
+    return render(request, 'documents/documents_details.html', {
+        'document': document,
+        'comments': comments,
+        'form': comment_form
+    })
 
 def documents_upload(request):
     if request.method == 'POST':
