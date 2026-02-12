38a8c0d9a1fe236c7d3d22c221e2cc48cef29e69
caioduart3 <caioduarte330@gmail.com>
2026-02-12 11:01:59 -0300

    style: estilização login

--- docs_manager/apps/users/forms.py
+++ docs_manager/apps/users/forms.py
@@ -1,9 +1,18 @@
 from django import forms
-from .models import User
+from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'input'
+        })
+    )
 
     class Meta:
         model = User
         fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'input'
+            }),
+        }
